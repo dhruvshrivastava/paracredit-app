ca36522ba4eb4d00efc432713906cde1b6abfbbd
Dhruv Shrivastava <dhruvsri5@gmail.com>
2024-09-24 01:29:48 +0530

    add java skeleton

---
+++ paracredit/src/main/java/com/paracredit/paracredit/ParacreditApplication.java
@@ -0,0 +1,13 @@
+package com.paracredit.paracredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParacreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParacreditApplication.class, args);
+	}
+
+}
---
+++ paracredit/src/test/java/com/paracredit/paracredit/ParacreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paracredit.paracredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParacreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
